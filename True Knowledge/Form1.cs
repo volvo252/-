20bf014a5b7b17278bdd25d61388eee23d121afb
volvo252 <roma53591@yandex.ru>
2023-05-25 17:09:09 +0300

    Добавьте файлы проекта.

---
+++ True Knowledge/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+
+namespace True_Knowledge
+{
+    public partial class Form1 : Form
+    {
+        private const string APP_NAME = "ПРЕДСКАЗАНИЯ БИСПЛАТНА";
+        private readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\predictionsConfig.json";
+        private string[] _predictions;
+        private Random _random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            bPredict.Enabled = false;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        UpdateProgressBar(i);
+                        this.Text = $"{i}%";
+                    }));
+                    Thread.Sleep(10);
+                }
+            });
+            var index = _random.Next(_predictions.Length);
+
+            var prediction = _predictions[index];
+
+            MessageBox.Show(prediction);
+
+            progressBar1.Value = 0;
+            this.Text = APP_NAME;
+            bPredict.Enabled = true;
+
+        }
+
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+        progressBar1.Value = i;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = APP_NAME;
+
+            try
+            {
+                var data = File.ReadAllText(PREDICTIONS_CONFIG_PATH);
+
+                _predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (_predictions == null)
+                {
+                    Close();
+                }
+                else if (_predictions.Length == 0)
+
+                {
+                    MessageBox.Show("Бэбру саси, предсказаний не завезли");
+                    Close();
+                }
+            }
+        }
+    }
+}
---
+++ True Knowledge/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("True Knowledge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("True Knowledge")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2e8b13d8-41e0-4a15-9611-56e5dadf5b9b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
